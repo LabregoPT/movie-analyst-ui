258aba9d1bd398cbc95b7d9802f4f58767e7ffdd
LabregoPT <jhonedmora@gmail.com>
2021-06-04 23:37:58 -0500

    Fix and test

--- server.js
+++ server.js
@@ -75,3 +75,5 @@ app.get('/pending', function(req, res){
 })
 
 app.listen(3030);
+
+module.exports = http.createServer();
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = "test";
 //Require the dev-dependencies
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-//let server = require("../server");
+let server = require("../server");
 let should = chai.should();
 
 chai.use(chaiHttp);
@@ -15,7 +15,7 @@ describe("API", () => {
   describe("/GET home", () => {
     it("it should GET any reply", done => {
       chai
-		chai.request('http://localhost:3030')
+		.request('http://localhost:3030')
         //.request(server)
         .get("/")
         .end((err, res) => {
